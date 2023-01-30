bf8f3352b3a3c98f0d2c42c29dd2cf838db1b71e
Vivek100000 <94130401+Vivek100000@users.noreply.github.com>
2023-01-30 20:11:22 +0530

    commited 30-jan 2023

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyttsx3,PyPDF2
+
+PdfReader = PyPDF2.PdfReader(open('book.pdf','rb'))
+speaker = pyttsx3.init()
+
+for page_num in range(len(PdfReader.pages)):
+    text = PdfReader.pages[page_num].extract_text()
+    processed_text = text.strip().replace('\n',' ')
+
+
+speaker.save_to_file(processed_text,'mp3_fromat.mp3')
+speaker.runAndWait()
+
+speaker.stop()
